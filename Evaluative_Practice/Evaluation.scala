25a4d274b64cb928d4a82761d6bcc314f8fbabe1
rbnzuiga <ruben.zuniga16@tectijuana.edu.mx>
2021-04-15 09:53:52 -0700

    second half exam

--- Evaluative_Practice/Evaluation.scala
+++ Evaluative_Practice/Evaluation.scala
@@ -32,3 +32,32 @@ dataframe.select("Date", "Close").orderBy(desc("Close")).show(1)
 It refers to how  Netflix ends the day with its finances taking into 
 account its High and lows
 */
+// 10. What is the maximum and minimum of the “Volume” column?
+
+val maxVolume = dataframe.agg(Map("Volume" -> "max"))
+maxVolume.show()
+val minVolume = dataframe.agg(Map("Volume" -> "min"))
+minVolume.show()
+
+//11.With Scala / Spark $ syntax answer the following:
+// ◦ Hint: Basically very similar to the dates session, you will have to create another
+// dataframe to answer some of the items.
+val df3=dataframe
+df3.show()
+
+//a. How many days was the “Close” column less than $ 600?
+val infcl = df3.filter($"Close" < 600 ).count()
+
+//b. What percentage of the time was the “High” column greater than $ 500?
+val dfporcentaje = df3.filter($"High" > 500).count()
+val resultado: Double  = (dfporcentaje*100)/(dataframe.count)
+
+//c. What is the Pearson correlation between column "High" and column "Volume"?
+df3.stat.corr("High", "Volume")
+df3.select(corr($"High", $"Volume")).show()
+
+//d. What is the maximum in the “High” column per year?
+df3.groupBy(year(dataframe("Date"))).max("High").show()
+
+//e. What is the “Close” column average for each calendar month?
+val clavg=df3.groupBy(month(dataframe("Date"))).avg("Close")

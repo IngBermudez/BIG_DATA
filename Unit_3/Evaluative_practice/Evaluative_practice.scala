e645bd8e58fbd052cc761271e67d789e1f2c213c
rbnzuiga <ruben.zuniga16@tectijuana.edu.mx>
2021-06-15 17:32:44 -0700

    Evaluative practice unit 3 scala part2

--- Unit_3/Evaluative_practice/Evaluative_practice.scala
+++ Unit_3/Evaluative_practice/Evaluative_practice.scala
@@ -18,3 +18,21 @@ val dataset = spark.read.option("header","true").option("inferSchema","true").cs
 
 //6. Select the following columns: Fresh, Milk, Grocery, Frozen, Detergents_Paper, Delicassen and call this set feature_data
 val feature_data = dataset.select("Fresh", "Milk", "Grocery", "Frozen", "Detergents_Paper", "Delicassen")
+
+//7. Import Vector Assembler and Vector
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
+//8. Create a new Vector Assembler object for the feature columns as an input set, remembering that there are no labels
+val VectorA = new VectorAssembler().setInputCols (Array ("Fresh", "Milk", "Grocery", "Frozen", "Detergents_Paper", "Delicassen")). setOutputCol ("features")
+
+//9. Use the assembler object to transform feature_data
+val output= VectorA.transform(feature_data)
+//10. Create a Kmeans model with K = 3
+val kmeans = new KMeans().setK(3).setSeed(1L)
+val model = kmeans.fit(output)
+//11. Evaluate groups using Within Set Sum of Squared Errors WSSSE and print centroids.
+val WSSE = model.computeCost(output)
+println(s"Within set sum of Squared Errors = $WSSE")
+
+println("Cluster Centers: ")
+model.clusterCenters.foreach(println)
